a38bf20e753f215579467e2e16380426b06b2c35
Ahkillah Davis <ahkillahdavis@Ahkillahs-Air.zyxel.com>
2016-10-28 10:33:48 -0500

    added first p tag to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+  console.log( "Hey this works!" );
+
+  var buttonClick = function(){
+    alert( 'Button clicked' )
+  };
+
+  var showAlert = function(){
+    alert( "onclick event completed" )
+  };
